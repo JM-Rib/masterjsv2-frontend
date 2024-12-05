ff13088a3ae5702106b18c7fb91921b1cce22a56
JM_Rib <jeanmarin.ribaric@gmail.com>
2024-12-05 17:49:52 +0100

    ajout navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Accueil from './pages/Accueil';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <Navbar />
+          <BrowserRouter>
+            <Routes>
+                <Route
+                  path="/"
+                  element={<Accueil />}
+                />
+            </Routes>
+          </BrowserRouter>
+      </div>
   );
 }
 
